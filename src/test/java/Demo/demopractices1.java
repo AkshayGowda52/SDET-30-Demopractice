3839166d15c5f2d998c7af18a357494b7c59b602
aksha <aksha@Akshay>
2022-03-10 15:27:56 +0530

    commit and push

---
+++ src/test/java/Demo/demopractices1.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class demopractices1 
+{
+  @Test
+  public void demo()
+  {
+	  System.out.println("user 1 - first commit");
+  }
+  
+}
